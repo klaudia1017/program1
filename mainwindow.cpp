f01d8995a167e92a175facdb27af833deaa07dd6
Unknown <mikolaj.balcerzak@student.put.poznan.pl>
2018-01-10 08:58:20 +0100

    spinBox to TextBox
    ...

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,9 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    qint8 x = ui->spinBox->value();
+    ui->textBox1->setText(QString::number(x));
+}
