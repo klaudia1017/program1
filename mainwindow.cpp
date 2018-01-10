3b306b44f03a5e0a4aef6c98e55510618d7a56d7
Unknown <mikolaj.balcerzak@student.put.poznan.pl>
2018-01-10 11:00:35 +0100

    All functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,5 +17,20 @@ void MainWindow::on_pushButton_clicked()
 {
     qint8 x1 = ui->spinBox->value();
     qint8 x2 = ui->spinBox_2->value();
-    ui->textBox1->setText(QString::number(x1*x2));
+    if (ui->comboBox->currentText()=="x")
+        {
+            ui->textBox1->setText(QString::number(x1*x2));
+        }
+    else if (ui->comboBox->currentText()=="+")
+        {
+            ui->textBox1->setText(QString::number(x1+x2));
+        }
+    else if (ui->comboBox->currentText()=="-")
+        {
+            ui->textBox1->setText(QString::number(x1-x2));
+        }
+    else if ((ui->comboBox->currentText()=="/")&(x2!=0))
+        {
+            ui->textBox1->setText(QString::number(x1/x2));
+        }
 }
